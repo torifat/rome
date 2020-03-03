ab002e30cced4d14d5efca7a330a329ceb32b7b7
Rifat Nabi <to.rifat@gmail.com>
2020-03-03 22:50:36 +1100

    WIP

---
+++ packages/@romejs/js-parser/__rtests__/__rfixtures__/typescript/import/type/input.ts
@@ -0,0 +1,7 @@
+import type Def1 from "foo";
+import type {named1} from "foo";
+import type Def2, {named2} from "foo";
+import type switch1 from "foo";
+import type { switch2 } from "foo";
+import type { foo1, bar1 } from "baz";
+import type from "foo";
